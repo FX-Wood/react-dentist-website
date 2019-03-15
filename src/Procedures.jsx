85f403fd599c10f603f62c20c8efec77a953203e
FX-Wood <fxwood2@gmail.com>
2019-03-15 12:17:09 -0700

    Implemented props

--- src/Procedures.jsx
+++ src/Procedures.jsx
@@ -5,13 +5,9 @@ export default class Procedures extends Component {
         return (
             <div className="procedures">
             <h1>Things we do that hurt</h1>
-            <ul>
-                <li>Root Canals</li>
-                <li>Deep Root Canals</li>
-                <li>Scraping with metal hooks</li>
-                <li>Fillings</li>
-                <li>Cleanings</li>
-            </ul>
+                <ul>
+                    {this.props.tx.map( (name, i) => <li key={i}>{name}</li> )}
+                </ul>
             </div>
         )
     }
