7d80dd56228b345b813f7b76c1affec7dff4655a
FX-Wood <fxwood2@gmail.com>
2019-03-15 12:20:50 -0700

    Refactored to functional component

--- src/Procedures.jsx
+++ src/Procedures.jsx
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 
-export default class Procedures extends Component {
-    render() {
-        return (
-            <div className="procedures">
-            <h1>Things we do that hurt</h1>
-                <ul>
-                    {this.props.tx.map( (name, i) => <li key={i}>{name}</li> )}
-                </ul>
-            </div>
-        )
-    }
+export default function Procedures(props) {
+    return (
+        <div className="procedures">
+        <h1>Things we do that hurt</h1>
+            <ul>
+                {props.tx.map( (name, i) => <li key={i}>{name}</li> )}
+            </ul>
+        </div>
+    )
 }
