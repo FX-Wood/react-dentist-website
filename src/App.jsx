0a4a3a482e8843fc9226fd1ccd3ba32a80396932
FX-Wood <fxwood2@gmail.com>
2019-03-15 12:16:48 -0700

    Adding react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
+import Home from './Home';
+import Procedures from './Procedures';
+import Contact from './Contact';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
 
 class App extends Component {
   render() {
+    const tx = [
+      'Deep Clean',
+      'Filling',
+      'Crown',
+      'Root Canal',
+      'Deep Route Canal'
+    ]
     return (
-      <div className="App">
-        <p>
-          Foooooooooooooo! Farge!
-        </p>
-      
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <nav>
+            <Link to='/'>Home</Link> |{' '}
+            <Link to='/procedures'>Procedures</Link> |{' '}
+            <Link to='/contact'>Contact</Link>
+          </nav>
+          <Route exact path='/' render={() => <Home />} />
+          <Route path='/procedures' render={() => <Procedures tx={tx}/>} />
+          <Route path='/contact' render={() => Contact} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
